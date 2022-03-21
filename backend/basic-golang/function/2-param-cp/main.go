99406484f77068ca28b7bb6bfb10dc8c2957f96c
Damara Syaidil <damaraccd11@gmail.com>
2022-03-21 17:01:11 +0700

    Complete Assignment

--- backend/basic-golang/function/2-param-cp/main.go
+++ backend/basic-golang/function/2-param-cp/main.go
@@ -5,9 +5,12 @@ import "fmt"
 //memanggil fungsi goodMorning
 //fungsi goodMorning akan melakukan print selamat pagi + name yang didapat dari parameter fungsi
 func main() {
-	goodMorning("teman")
-	goodMorning("teman 2")
+	goodMorning("Damara")
+	goodMorning("Anto")
 
 }
 
 // TODO: answer here
+func goodMorning(name string) {
+	fmt.Println("Hello Good Morning", name)
+}
