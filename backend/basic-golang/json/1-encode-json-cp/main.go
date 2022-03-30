e6d3302b3d4a109447e2a9cfe47ade3f8b409d77
Damara Syaidil <damaraccd11@gmail.com>
2022-03-30 09:29:25 +0700

    uncomplete assignment

--- backend/basic-golang/json/1-encode-json-cp/main.go
+++ backend/basic-golang/json/1-encode-json-cp/main.go
@@ -1,5 +1,10 @@
 package main
 
+import (
+	"encoding/json"
+	"log"
+)
+
 // Dari struct dan nama field yang sama dari contoh
 // Buat string JSON dengan hasil seperti berikut
 // {"jenis":"Meja Belajar","color":"green","jumlah":2}
@@ -12,7 +17,12 @@ type Meja struct {
 }
 
 func (m Meja) EncodeJSON() string {
-	// TODO: answer here
+	meja := NewMeja(Meja{Jenis: "Meja Belajar", Warna: "green", Jumlah: 2})
+	mejaJSON, err := json.Marshal(meja)
+	if err != nil {
+		log.Fatal("JSON Marshal error: ", err)
+	}
+	return string(mejaJSON)
 }
 
 func NewMeja(m Meja) Meja {
