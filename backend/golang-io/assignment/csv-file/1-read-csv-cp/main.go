a3bab238ed99da70517a39a71a2f179592136af6
Damara Syaidil <damaraccd11@gmail.com>
2022-04-05 12:10:56 +0700

    Finish Assignment

--- backend/golang-io/assignment/csv-file/1-read-csv-cp/main.go
+++ backend/golang-io/assignment/csv-file/1-read-csv-cp/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/csv"
 	"fmt"
+	"io"
 	"os"
 )
 
@@ -11,5 +12,24 @@ func main() {
 }
 
 func CSVToMap(data map[string]string, fileName string) (map[string]string, error) {
-	// TODO: answer here
+	//TODO: Answer Here
+	file, err := os.Open(fileName)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	reader := csv.NewReader(file)
+	for {
+		record, err := reader.Read()
+		if err == io.EOF {
+			break
+		} else if err != nil {
+			return nil, err
+		}
+
+		data[record[0]] = record[1]
+	}
+
+	return data, nil
 }
