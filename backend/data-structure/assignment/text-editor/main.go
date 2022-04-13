1d08d2bd3af854d0513be9ea54b62754d23c7f04
Damara Syaidil <damaraccd11@gmail.com>
2022-04-13 22:04:08 +0700

    Finish Assignment text-editor

--- backend/data-structure/assignment/text-editor/main.go
+++ backend/data-structure/assignment/text-editor/main.go
@@ -27,16 +27,47 @@ func NewTextEditor() *TextEditor {
 
 func (te *TextEditor) Write(ch rune) {
 	// TODO: answer here
+	te.UndoStack.Data = append(te.UndoStack.Data, ch)
+
+	for i := 0; i < len(te.RedoStack.Data); i++ {
+		te.RedoStack.Data = te.RedoStack.Data[:i]
+	}
+
+	fmt.Println(te.UndoStack.Data)
+	fmt.Println(te.RedoStack.Data)
 }
 
 func (te *TextEditor) Read() []rune {
 	// TODO: answer here
+	if te.UndoStack.IsEmpty() {
+		return nil
+	}
+
+	return te.UndoStack.Data
 }
 
 func (te *TextEditor) Undo() {
 	// TODO: answer here
+	if te.UndoStack.IsEmpty() {
+		return
+	}
+
+	te.RedoStack.Data = append(te.RedoStack.Data, te.UndoStack.Data[len(te.UndoStack.Data)-1])
+	te.UndoStack.Data = te.UndoStack.Data[:len(te.UndoStack.Data)-1]
+
+	fmt.Println(te.UndoStack.Data)
+	fmt.Println(te.RedoStack.Data)
 }
 
 func (te *TextEditor) Redo() {
 	// TODO: answer here
+	if te.RedoStack.IsEmpty() {
+		return
+	}
+
+	te.UndoStack.Data = append(te.UndoStack.Data, te.RedoStack.Data[len(te.RedoStack.Data)-1])
+	te.RedoStack.Data = te.RedoStack.Data[:len(te.RedoStack.Data)-1]
+
+	fmt.Println(te.UndoStack.Data)
+	fmt.Println(te.RedoStack.Data)
 }
