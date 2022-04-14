9ba3282ff7483e19f1a1613a74296aeeb667a79f
Damara Syaidil <damaraccd11@gmail.com>
2022-04-14 21:44:46 +0700

    Finish Assignment Stack 4

--- backend/data-structure/stack/4-peek-cp/main.go
+++ backend/data-structure/stack/4-peek-cp/main.go
@@ -8,16 +8,35 @@ var ErrEmptyStack = errors.New("stack is empty")
 
 type Stack struct {
 	// TODO: answer here
+	Top  int
+	Data []int
 }
 
 func NewStack(size int) Stack {
 	// TODO: answer here
+	return Stack{
+		Top:  -1,
+		Data: []int{},
+	}
 }
 
 func (s *Stack) Push(Elemen int) error {
 	// TODO: answer here
+	if len(s.Data) == 10 {
+		return ErrEmptyStack
+	}
+
+	s.Top += 1
+	s.Data = append(s.Data, Elemen)
+
+	return nil
 }
 
 func (s *Stack) Peek() (int, error) {
 	// TODO: answer here
+	if s.Top == -1 {
+		return 0, ErrEmptyStack
+	}
+
+	return s.Data[s.Top], nil
 }
