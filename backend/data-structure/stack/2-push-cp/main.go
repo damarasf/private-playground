44af262372ea33e6e68ad3c338fdcff54666bcc1
Damara Syaidil <damaraccd11@gmail.com>
2022-04-14 21:31:09 +0700

    Finish Assignment Stack 2

--- backend/data-structure/stack/2-push-cp/main.go
+++ backend/data-structure/stack/2-push-cp/main.go
@@ -10,12 +10,28 @@ var ErrStackOverflow = errors.New("stack overflow")
 
 type Stack struct {
 	// TODO: answer here
+	Top  int
+	Size int
+	Data []int
 }
 
 func NewStack(size int) Stack {
 	// TODO: answer here
+	return Stack{
+		Top:  -1,
+		Size: size,
+		Data: []int{},
+	}
 }
 
 func (s *Stack) Push(Elemen int) error {
 	// TODO: answer here
+	if s.Top == s.Size-1 {
+		return ErrStackOverflow
+	}
+
+	s.Top += 1
+	s.Data = append(s.Data, Elemen)
+
+	return nil
 }
