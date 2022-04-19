2eb3d7b12927a40b49f7f01a3f54900119cf46d8
Damara Syaidil <damaraccd11@gmail.com>
2022-04-19 09:32:09 +0700

    Finish Assignment Debugging 1

--- backend/basic-golang/debugging/assignment/1-sum-all/main.go
+++ backend/basic-golang/debugging/assignment/1-sum-all/main.go
@@ -3,14 +3,15 @@ package main
 import "fmt"
 
 func main() {
-	arr := []int{0, 1, 2, 3}
+	// arr := []int{0, 1, 2, 3}
+	arr := []int{12, 5, 10, 1, 3}
 
 	res := SumAll(arr)
 	fmt.Println(res)
 
 	// Try correct answer:
-	// resCorrect := SumAllCorrect(arr)
-	// fmt.Println(resCorrect)
+	resCorrect := SumAllCorrect(arr)
+	fmt.Println(resCorrect)
 }
 
 func SumAll(arr []int) int {
@@ -22,5 +23,14 @@ func SumAll(arr []int) int {
 }
 
 func SumAllCorrect(arr []int) int {
-	return 0 // TODO: replace this
+	resCorrect := 0
+	for _, val := range arr {
+		resCorrect += val
+	}
+
+	if len(arr) == 0 {
+		return 0
+	}
+
+	return resCorrect
 }
