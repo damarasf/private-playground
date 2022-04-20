2a0a02a5928107cfa53d69800a32271484a726ca
Damara Syaidil <damaraccd11@gmail.com>
2022-04-20 13:34:03 +0700

    Finish CP concurrency workshop goroutine 4

--- backend/concurrency/workshop/goroutine/4-stop-loop-concurrently-cp/main.go
+++ backend/concurrency/workshop/goroutine/4-stop-loop-concurrently-cp/main.go
@@ -1,22 +1,12 @@
 package main
 
 import (
-	"fmt"
 	"time"
 )
 
 //bagaiman cara menghentikan loop ?
 func stopLoop(loop *bool) {
 	// TODO: answer here
-	a := 0
-	for a < 10 {
-		fmt.Println("loop")
-		a++
-	}
-
 	*loop = false
-
 	time.Sleep(200 * time.Millisecond)
-
-	fmt.Println("main stop")
 }
