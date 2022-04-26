e0db47f29cd358909e7408695342ad15ad75ca0c
Damara Syaidil <damaraccd11@gmail.com>
2022-04-26 10:59:23 +0700

    Finish CP concurrency race-condition-basic

---
+++ backend/concurrency/workshop/race-condition/race-condition-basic-cp/main.go
@@ -0,0 +1,31 @@
+package main
+
+import "sync"
+
+//gunakan channel untuk memberpaiki masalah race condition!
+func counter(output chan<- int) {
+
+	// TODO: answer here
+	c := make(chan int)
+	var wg sync.WaitGroup
+	count := 0
+	for i := 0; i < 1000; i++ {
+		wg.Add(1)
+		go func() {
+			//kirim 1 ke channel
+			// TODO: answer here
+			c <- 1
+			wg.Done()
+		}()
+	}
+	//mengubah nilai count menggunakan data dari channel
+
+	// TODO: answer here
+	go func() {
+		for {
+			count += <-c
+		}
+	}()
+	wg.Wait() // menunggu seluruh goroutine selesai berjalan
+	output <- count
+}
