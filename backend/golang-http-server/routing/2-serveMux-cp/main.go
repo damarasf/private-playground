fa9f20ae7448b89f2597b9fd44fe544a2a81acd9
Damara Syaidil <damaraccd11@gmail.com>
2022-05-10 08:33:07 +0700

    Finish CP Routing serverMux

--- backend/golang-http-server/routing/2-serveMux-cp/main.go
+++ backend/golang-http-server/routing/2-serveMux-cp/main.go
@@ -12,15 +12,40 @@ import (
 
 var TimeHandler = func(writer http.ResponseWriter, request *http.Request) {
 	// TODO: answer here
+	if request.Method != "GET" {
+		writer.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	t := time.Now()
+	expected := fmt.Sprintf("%v, %v %v %v", t.Weekday(), t.Day(), t.Month(), t.Year())
+	writer.WriteHeader(http.StatusOK)
+	writer.Write([]byte(expected))
 }
 
 var SayHelloHandler = func(w http.ResponseWriter, r *http.Request) {
 	// TODO: answer here
+	if r.Method != "GET" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	name := r.URL.Query().Get("name")
+	if name == "" {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(fmt.Sprintf("Hello there")))
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hello, %s!", name)))
 }
 
 func main() {
 	mux := http.NewServeMux()
 	// TODO: answer here
+	mux.HandleFunc("/time", TimeHandler)
+	mux.HandleFunc("/hello", SayHelloHandler)
 
 	server := http.Server{
 		Addr:    "localhost:8080",
