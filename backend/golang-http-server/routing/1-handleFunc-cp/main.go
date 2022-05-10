b95a3b6ee284e3d50eb4227ca675c2935e66ff33
Damara Syaidil <damaraccd11@gmail.com>
2022-05-10 08:28:03 +0700

    Finish CP Routing handleFunc

--- backend/golang-http-server/routing/1-handleFunc-cp/main.go
+++ backend/golang-http-server/routing/1-handleFunc-cp/main.go
@@ -11,10 +11,33 @@ import (
 
 var TimeHandler = func(writer http.ResponseWriter, request *http.Request) {
 	// TODO: answer here
+	if request.Method != "GET" {
+		writer.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	t := time.Now()
+	expected := fmt.Sprintf("%v, %v %v %v", t.Weekday(), t.Day(), t.Month(), t.Year())
+	writer.WriteHeader(http.StatusOK)
+	writer.Write([]byte(expected))
 }
 
 var SayHelloHandler = func(w http.ResponseWriter, r *http.Request) {
 	// TODO: answer here
+	if r.Method != "GET" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
+
+	name := r.URL.Query().Get("name")
+	if name == "" {
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(fmt.Sprintf("Hello there")))
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(fmt.Sprintf("Hello, %s!", name)))
 }
 
 func main() {
