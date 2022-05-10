f206c1d793a124133d0991bd36473d6475f2c4ae
Damara Syaidil <damaraccd11@gmail.com>
2022-05-10 08:24:54 +0700

    Finish CP servermux

--- backend/golang-http-server/multiplexing/1-servemux-cp/main.go
+++ backend/golang-http-server/multiplexing/1-servemux-cp/main.go
@@ -11,16 +11,43 @@ import (
 // Route kedua yaitu "/hello" yang menghandle SayHelloHandler
 
 func TimeHandler() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {} // TODO: replace this
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "GET" {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+
+		t := time.Now()
+		expected := fmt.Sprintf("%v, %v %v %v", t.Weekday(), t.Day(), t.Month(), t.Year())
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(expected))
+	} // TODO: replace this
 }
 
 func SayHelloHandler() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {} // TODO: replace this
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != "GET" {
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			return
+		}
+
+		name := r.URL.Query().Get("name")
+		if name == "" {
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte(fmt.Sprintf("Hello there")))
+			return
+		}
+
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(fmt.Sprintf("Hello, %s!", name)))
+	} // TODO: replace this
 }
 
 func GetMux() *http.ServeMux {
 	mux := http.NewServeMux()
 	// TODO: answer here
-	return mux
+	mux.HandleFunc("/time", TimeHandler())
+	mux.HandleFunc("/hello", SayHelloHandler())
 
+	return mux
 }
