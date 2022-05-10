62d04261dce0fbdc19ab6f8ab4ab149436efe594
Damara Syaidil <damaraccd11@gmail.com>
2022-05-10 08:09:39 +0700

    Finish CP response code

--- backend/golang-http-server/response-code/response-code-cp/main.go
+++ backend/golang-http-server/response-code/response-code-cp/main.go
@@ -25,11 +25,29 @@ func IsNameExists(name string) bool {
 func GetNameHandler() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		// TODO: answer here
+		if r.Method != "GET" {
+			http.Error(w, "Method is not allowed.", http.StatusMethodNotAllowed)
+			return
+		}
+
+		name := r.URL.Query().Get("name")
+		if name == "" {
+			http.Error(w, "Name is required.", http.StatusBadRequest)
+			return
+		}
+
+		if !IsNameExists(name) {
+			http.Error(w, "Name is not exists.", http.StatusNotFound)
+			return
+		}
+
+		fmt.Fprintf(w, "Hello, %s", name)
 	}
 }
 
 func GetMux() *http.ServeMux {
 	mux := http.NewServeMux()
 	// TODO: answer here
+	mux.HandleFunc("/name", GetNameHandler())
 	return mux
 }
