7f089be96ff5e59c20b6728a2afe083f88b2a1aa
Damara Syaidil <damaraccd11@gmail.com>
2022-05-11 09:11:52 +0700

    Finsih CP gin route grouping

--- backend/golang-http-server/gin/5-route-grouping-cp/main.go
+++ backend/golang-http-server/gin/5-route-grouping-cp/main.go
@@ -27,16 +27,32 @@ var movies = map[int]Movie{
 }
 
 var MovieListHandler = func(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{}) // TODO: replace this
+	c.JSON(http.StatusOK, gin.H{
+		"data": movies,
+	})
 }
 
 var MovieGetHandler = func(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{}) // TODO: replace this
+	id, _ := strconv.Atoi(c.Param("id"))
+	if movie, ok := movies[id]; ok {
+		c.JSON(http.StatusOK, gin.H{
+			"data": movie,
+		})
+	} else {
+		c.JSON(http.StatusNotFound, gin.H{
+			"error": "movie not found",
+		})
+	}
 }
 
 func GetRouter() *gin.Engine {
 	router := gin.Default()
 	// TODO: answer here
+	movies := router.Group("/movie")
+	{
+		movies.GET("/list", MovieListHandler)
+		movies.GET("/get/:id", MovieGetHandler)
+	}
 	return router
 }
 
