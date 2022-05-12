00ccbe2b3b232a151279f686c823cbfb9e7f6605
Damara Syaidil <damaraccd11@gmail.com>
2022-05-12 14:49:54 +0700

    Finish CP design-pattern builder-pattern

--- backend/design-patterns/creational/3-builder-cp/house/house-new.go
+++ backend/design-patterns/creational/3-builder-cp/house/house-new.go
@@ -15,9 +15,25 @@ type House struct {
 
 type HouseBuilder interface {
 	// TODO: answer here
+	buildWindow(numOfWindow int)
+	buildDoor()
+	buildGarage()
+	buildSwimmingPool()
 	getHouse() House
 }
 
 func NewBuilder(builderType string) HouseBuilder {
-	return nil // TODO: replace this
+	if builderType == "zimbabwe" {
+		return &zimbabweHouseBuilder{
+			house: House{
+				HasSwimmingPool: false,
+			},
+		} // cek zimbabweHouseBuilder.go
+	}
+	if builderType == "indonesia" {
+		return &indonesiaHouseBuilder{} // cek indonesiaBuilder.go
+	}
+
+	return nil
+	// return nil // TODO: replace this
 }
